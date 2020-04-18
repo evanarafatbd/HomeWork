30e13bff4113f0a636b664947d8a99f5900dce48
Evan Arafat <evanarafatbd@aol.com>
2020-04-18 09:50:12 -0400

    4/18/2020

---
+++ Develop/MyFirstJavaProgram2.java
@@ -0,0 +1,9 @@
+
+public class MyFirstJavaProgram2 {
+/* i am attempting to out put my name here
+	attempt # 2 */
+	public static void main(String[] args) {
+	System.out.println("Evan Arafat NYC"); /*java used the general syntax 
+	object.method(parameters);*/	
+	}
+}
